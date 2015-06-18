75e742f352c8689c42859bad7124091f0c09309a
dalcausin <hi@dalcausin.com>
2015-06-17 21:23:05 -0700

    Conditionals exercise, slider update, created webapp folder

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -4,16 +4,12 @@ Element.prototype.slider = function() {
     var slider = this;
     var wrapper = slider.children[0];
     var slides = wrapper.children;
-    var position = 1;
+    var position = 0;
     var width = window.innerWidth;
     var leftButton = document.createElement('div');
     var rightButton = document.createElement('div');
 
-
-    var init = function(){
-
-        wrapper.style.width = slides.length * width + 'px';
-        wrapper.style.height = '100%';
+    this.createButtons = function(){
 
         leftButton.classList.add('left');
         rightButton.classList.add('right');
@@ -21,24 +17,51 @@ Element.prototype.slider = function() {
         slider.appendChild(leftButton);
         slider.appendChild(rightButton);
 
+        rightButton.addEventListener('mousedown',function(){
+
+          if (position > (width * (slides.length - 1)) * -1){
+            position = position - width;
+            wrapper.style.marginLeft = position +'px';
+          }
+
+        });
+
+        leftButton.addEventListener('mousedown',function(){
+
+          if (position < 0) {
+            position = position + width;
+            wrapper.style.marginLeft = position +'px';
+          }
+
+        });
+
+    };
+
+    this.resize = function (){
+
+        width = window.innerWidth;
+
+        wrapper.style.width = slides.length * width + 'px';
+        wrapper.style.height = '100%';
+
         for(var i=0; i<slides.length; i++) {
           slides[i].style.width = width + 'px';
         };
 
-        leftButton.addEventListener('mousedown',function(){
-          wrapper.style.marginLeft = width * position * -1 +'px';
-          position = position + 1;
-        });
-
-        rightButton.addEventListener('mousedown',function(){
-          wrapper.style.marginLeft = width * position * -1 +'px';
-          position = position - 1;
-        });
+    };
 
 
+    this.init = function(){
+
+        this.createButtons();
+        this.resize();
+
+        window.addEventListener('resize',slider.resize);
 
     };
 
-    init();
+    this.init();
+
 };
+
 /* end Slider */
