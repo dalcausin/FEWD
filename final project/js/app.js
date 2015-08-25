701c4aa483c15223f8a074f5481111bea9757b82
dalcausin <hi@dalcausin.com>
2015-08-25 11:13:41 +0200

    FEWD final project update

--- final project/js/app.js
+++ final project/js/app.js
@@ -182,6 +182,7 @@ this.getLocation = function () {
         function callback(results, status) {
           if (status == google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
+              console.log(results);
               createMarker(results[i]);
               // createList(results[i]);
           }
